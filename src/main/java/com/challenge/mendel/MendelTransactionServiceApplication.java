df94402e119ac1fd248b20411229014c84c3e73c
Fedegewis <fedegewis@gmail.com>
2026-05-27 10:49:13 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/challenge/mendel/MendelTransactionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.challenge.mendel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MendelTransactionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MendelTransactionServiceApplication.class, args);
+    }
+}
